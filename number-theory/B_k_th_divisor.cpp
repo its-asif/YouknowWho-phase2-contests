95982a270ad9f0f9fbb483a1b789962fda651f5c
its-asif <asifiscoding@gmail.com>
2023-04-07 16:43:50 +0600

    first commit

---
+++ number-theory/B_k_th_divisor.cpp
@@ -0,0 +1,25 @@
+/*
+Basic Number Theory (B) | YouKn0wWho Academy
+    CodeForces - 762A 
+    k-th divisor 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    ll n,k; cin>>n>>k;
+    vector<ll> v;
+    for (ll i = 1; i*i <= n; i++)
+    {
+        if(n%i==0){
+            v.push_back(i);
+            if(i!=n/i)v.push_back(n/i);
+        }
+    }
+   if(k>v.size()) cout<<-1;
+   else{
+    sort(v.begin(),v.end());
+    cout<<v[k-1];
+   } 
+}
