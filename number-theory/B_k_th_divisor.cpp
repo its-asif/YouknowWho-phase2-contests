211d54c1373908eda5e9e19095491ece3821bd4f
its-asif <asifiscoding@gmail.com>
2023-04-07 16:51:12 +0600

    CodeForces - 762A

--- number-theory/B_k_th_divisor.cpp
+++ number-theory/B_k_th_divisor.cpp
@@ -12,7 +12,7 @@ int main(){
     vector<ll> v;
     for (ll i = 1; i*i <= n; i++)
     {
-        if(n%i==0){
+        if(n%i==0){ 
             v.push_back(i);
             if(i!=n/i)v.push_back(n/i);
         }
