cfc9c2847d2b6eb424df2f4256fcf943c7572a87
its-asif <asifiscoding@gmail.com>
2023-04-13 01:25:47 +0600

    SPOJ - TDPRIMES : Optimized Sieve

---
+++ G_Optimized_Sieve.cpp
@@ -0,0 +1,46 @@
+/*
+Basic Number Theory (G) | YouKn0wWho Academy
+    SPOJ - TDPRIMES
+    Optimized Sieve
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define testcase ll t; cin>>t; while(t--)
+
+bool isSquare(ll x){
+    if(sqrt(x)== (int)sqrt(x)) return true;
+    else return false;
+}
+
+bool isPrime(ll x){
+    for(ll i=2; i*i <= x; i++)
+        if(x%i==0) return false;
+
+    return true;
+}
+
+const int N = 1e8+69;
+bool nums[N];
+vector <int > primes;
+void sieve(){
+    for( int i=2; i*i<= N; i++){
+        if(nums[i]) continue;
+        for(int j = i*i; j <= N; j+= i)
+            nums[j] = true;
+    }
+    for(int i=2;i<=N; i++)
+      if(!nums[i]) primes.push_back(i);
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+    nums[0] = nums[1] = true;
+    sieve();
+  for(int i=0; i<= primes.size(); i+= 100)
+      cout<<primes[i]<<endl;
+        
+}
