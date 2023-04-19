928dc033b825303b14d8afd7ac8108aa54a4c0bf
its-asif <asifiscoding@gmail.com>
2023-04-20 03:16:51 +0600

    CodeForces - 1370A : Maximum GCD

---
+++ math/N_Maximum_GCD.cpp
@@ -0,0 +1,22 @@
+/*
+Basic Math And Again Number Theory(N) | YouKn0wWho Academy
+    CodeForces - 1370A 
+    Maximum GCD  
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define testcase int t; cin>>t; for(int tt=1;tt<=t;tt++)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/__gcd(a,b)
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    testcase{
+        int n; cin>>n;
+        cout<< n/2 << endl;
+    }
+    return 0;
+}
