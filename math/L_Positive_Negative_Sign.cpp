235abb09ec4a5a0a830f02942854ce62bdc56c92
its-asif <asifiscoding@gmail.com>
2023-04-20 03:04:41 +0600

    LightOJ - 1294 : Positive Negative Sign

---
+++ math/L_Positive_Negative_Sign.cpp
@@ -0,0 +1,24 @@
+/*
+Basic Math And Again Number Theory(L) | YouKn0wWho Academy
+    LightOJ - 1294 
+    Positive Negative Sign  
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define testcase int t; cin>>t; for(int tt=1;tt<=t;tt++)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/__gcd(a,b)
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    testcase{
+        ll n,m; cin>> n >> m;
+        cout<<"Case "<<tt<<": ";
+        cout<< (n / 2) * m << endl;
+    }
+    return 0;
+}
